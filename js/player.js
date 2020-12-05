3e2a583fc43393e9b2a52e972be191746df506da
Kim Brown <kbrownc341@gmail.com>
2020-12-05 18:00:49 -0500

    Initial commit

---
+++ js/player.js
@@ -0,0 +1,10 @@
+let player;
+
+function Player(classType, health, mana, strength, agility, speed) {
+	this.classtype = classType;
+	this.health = health;
+	this,mana = mana;
+	this.strength = strength;
+	this.agility = agility;
+	this.speed = speed;
+}
