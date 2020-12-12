18baef057dd0d5a4dd7a1de57815dd422efe54bc
Kim Brown <kbrownc341@gmail.com>
2020-12-12 12:22:04 -0500

    After episode 24

--- js/player.js
+++ js/player.js
@@ -1,9 +1,9 @@
 let player;
 
 function Player(classType, health, mana, strength, agility, speed) {
-	this.classtype = classType;
+	this.classType = classType;
 	this.health = health;
-	this,mana = mana;
+	this.mana = mana;
 	this.strength = strength;
 	this.agility = agility;
 	this.speed = speed;
